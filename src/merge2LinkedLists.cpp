e94cb73a9ff053af399245bc8fdcf04a5b6c7848
adityagampa <adityagampa@missionrnd.com>
2016-02-01 10:45:55 +0530

    Submit Commit

--- src/merge2LinkedLists.cpp
+++ src/merge2LinkedLists.cpp
@@ -19,52 +19,54 @@ struct node {
 };
 
 
-struct node *merge2LinkedLists(struct node *head, struct node *temp)
+struct node *merge2LinkedLists(struct node *p, struct node *q)
 {
-	struct node *prev = head, *m = head;
-	int flag = 0;
-	if (!temp)
-		return head;
-	if (!head)
-		return temp;
-	if (head->num > temp->num)
+	struct node *t, *prev, *head;
+	if (!p && !q)
+		return NULL;
+	if (!p)
+		return q;
+	if (!q)
+		return p;
+	if (p->num > q->num)
 	{
-		m = temp;
-		temp = head;
+		head = q;
+		t = q;
+		q = q->next;
 	}
-	while (m && temp)
+	else
 	{
-		if (temp->num < m->num && flag == 0)
-		{
-			prev->next = temp;
-			prev = temp;
-			temp = temp->next;
-			flag = 1;
-		}
-		else if (flag && m->num < temp->num)
-		{
-			prev->next = m;
-			prev = m;
-			m = m->next;
-			flag = 0;
-		}
-		else if (flag == 0)
-		{
-			prev = m;
-			m = m->next;
-		}
-		else if (flag)
-		{
-			prev = temp;
-			temp = temp->next;
-		}
-
+		head = p;
+		t = p;
+		p = p->next;
 	}
-	if (m)
+	while (p && q)
 	{
-		prev->next = m;
+		if (p->num > q->num)
+		{
+			t->next = q;
+			t = q;
+			q = q->next;
+		}
+		else if (p->num < q->num)
+		{
+			t->next = p;
+			t = p;
+			p = p->next;
+		}
+		else
+		{
+			t->next = p;
+			t = p;
+			p = p->next;
+			t->next = q;
+			t = q;
+			q = q->next;
+		}
 	}
-	else if (temp)
-		prev->next = temp;
+	if (q)
+		t->next = q;
+	else 
+		t->next = p;
 	return head;
 }
